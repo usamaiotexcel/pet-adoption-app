7abf7d244584b367f0e88fd69a980da36b93cd41
usamaiotexcel <usama.iotexcel@gmail.com>
2024-05-30 12:00:43 +0530

    Initial commit with changes

--- lib/widgets/stories_section.dart
+++ lib/widgets/stories_section.dart
@@ -8,7 +8,8 @@ class StoriesSection extends StatefulWidget {
   State<StoriesSection> createState() => _StoriesSectionState();
 }
 
-class _StoriesSectionState extends State<StoriesSection> with SingleTickerProviderStateMixin {
+class _StoriesSectionState extends State<StoriesSection>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<Color?> animation;
   @override
@@ -26,54 +27,55 @@ class _StoriesSectionState extends State<StoriesSection> with SingleTickerProvid
         setState(() {});
       });
   }
+
   @override
   dispose() {
     _controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    List storiesList = ['Tuna', 'Jacob', 'Sophie', 'Hachiko', 'Jony'];
+    List storiesList = ['Tuna', 'Jacob', 'Sophie', 'Hachiko', 'Jonie'];
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
         children: storiesList
             .map<Widget>((story) => TweenAnimationBuilder<double>(
-            tween: Tween(begin: 0, end: 1),
-            duration: const Duration(seconds: 1),
-            builder: (context, value, _) {
-              _controller.forward();
-              return Container(
-                      margin: const EdgeInsets.only(right: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: value*65,
-                            width: value*65,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                    color: storiesList.indexOf(story) == 4
-                                        ? Colors.grey.shade400
-                                        : animation.value!,
-                                    width: value*3),
-                                image: DecorationImage(
-                                    image: AssetImage(
-                                        'assets/stories/${(storiesList.indexOf(story) + 1)}.png'),
-                                    fit: BoxFit.cover)),
-                          ),
-                          const Gap(5),
-                          Text(
-                            story,
-                            style: const TextStyle(
-                                fontSize: 11, fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
-                    );
-              }
-            ))
+                tween: Tween(begin: 0, end: 1),
+                duration: const Duration(seconds: 1),
+                builder: (context, value, _) {
+                  _controller.forward();
+                  return Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: value * 65,
+                          width: value * 65,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                  color: storiesList.indexOf(story) == 4
+                                      ? Colors.grey.shade400
+                                      : animation.value!,
+                                  width: value * 3),
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/stories/${(storiesList.indexOf(story) + 1)}.png'),
+                                  fit: BoxFit.cover)),
+                        ),
+                        const Gap(5),
+                        Text(
+                          story,
+                          style: const TextStyle(
+                              fontSize: 11, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  );
+                }))
             .toList(),
       ),
     );
